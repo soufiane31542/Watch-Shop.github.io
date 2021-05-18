551b031eccfe7c45677a07494fe44d4157064d4d
soufiane31542 <84422943+soufiane31542@users.noreply.github.com>
2021-05-19 00:37:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+(const openNav = document.querySelector(".hamburger");
+const closeNav = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+
+const leftPosition = menu.getBoundingClientRect().left;
+openNav.addEventListener("click", () => {
+  if (leftPosition < 0) {
+    menu.classList.add("show");
+  }
+});
+
+closeNav.addEventListener("click", () => {
+  if (leftPosition < 0) {
+    menu.classList.remove("show");
+  }
+});
+
+
